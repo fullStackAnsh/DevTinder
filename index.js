b475e4c5e937dfd107b5e9535b8d4c3f1996d27d
fullStackAnsh <anshkanojia13@gmail.com>
2025-10-14 22:23:54 +0530

    Clean: remove unnecessary tracked files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const app=express()
 
-
+app.use('/',(req,res)=>{
+    res.send("Hello");
+});
 
 app.listen(3000,()=>{
     console.log("This server is running at port 3000");
