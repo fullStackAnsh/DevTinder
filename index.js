d3726954533e62c783a833dc8043a19feabc2c6b
fullStackAnsh <anshkanojia13@gmail.com>
2025-10-15 09:55:22 +0530

    Exploring try-catch error handling .

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app=express()
 
 //Dummy Middleware adminAuth is called 
 app.get('/admin/getAllData',adminAuth,(req,res)=>{
+    try{
+    //Throwing error will redirect to catch block      
+    throw new Error('xyz')    
     res.send("Fetching all data")
+   }
+   catch(err){
+    //This block will handle the error
+    res.send("Somethng wrong happened!");
+   }
 })
 
 app.listen(3000,()=>{
